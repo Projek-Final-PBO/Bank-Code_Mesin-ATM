5b1162b0d32c6758d92db01a214f4cf63aaba4fa
Khairul2991 <auni3934@gmail.com>
2022-11-03 10:59:51 +0700

    menambah hai dan 1

--- hai.java
+++ hai.java
@@ -4,6 +4,7 @@
 public class hai {
 
     public static void main(String[] args) {
-        System.out.println("papma");
+        System.out.println("hai");
+        System.err.println(1);
     }
 }
